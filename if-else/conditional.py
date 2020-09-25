a71269d1afec5ef6e8e0d8526f4ea97814c80004
Shyngys <Shyngys>
2020-09-25 21:33:08 +0600

    solver

--- if-else/conditional.py
+++ if-else/conditional.py
@@ -1,16 +1,32 @@
 import random
 
 def is_positive(num):
+    if num>=0:
+        return true
+    else:
+        return false
     # return true if num is positive, otherwise return false
 
 def is_even(num):
+    if num%2 == 0:
+        return true
+    else:
+        return false
     # return true if num is even, otherwise return false
 
 
 def is_positive_and_even(num):
+    if num>=0 && num%2 == 0:
+        return true
+    else:
+        return false
     # return true if num is positive and even, otherwise return false
 
 
 def is_positive_or_even(num):
+    if num>=0 || num%2 == 0:
+        return true
+    else:
+        return false
     # return true if num is positive or even, otherwise return false
 
